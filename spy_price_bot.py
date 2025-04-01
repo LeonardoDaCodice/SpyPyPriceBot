25c521763255aaa86322547c1a77636dfb63a206
LeonardoDaCodice <fortesalvatore333@gmail.com>
2025-04-01 18:04:00 +0200

    Updated Selenium configuration and Dockerfile for Render deployment

--- spy_price_bot.py
+++ spy_price_bot.py
@@ -30,9 +30,10 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.binary_location = "/usr/bin/chromium"  # Aggiungi questa linea per definire il percorso di Chromium
 
-# Inizializza il WebDriver
-service = Service("/usr/lib/chromium-driver/chromium-driver")
+# Inizializza il WebDriver utilizzando ChromeDriverManager per gestire la versione del driver
+service = Service(ChromeDriverManager().install())  # Usa ChromeDriverManager per ottenere il driver corretto
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 
